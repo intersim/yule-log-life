a283ff22b3ef99020ec069d363a2c3c7d48801c3
intersim <emily.intersimone@gmail.com>
2017-10-17 21:42:49 -0400

    small tweaks to player

--- public/script.js
+++ public/script.js
@@ -6,7 +6,9 @@ const [ iframe ] = Array.from(document.getElementsByTagName('iframe'));
 iframe.setAttribute('width', width);
 iframe.setAttribute('height', height);
 
-iframe.setAttribute('src', videos[0]);
+const url = videos[0] + '?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1';
+
+iframe.setAttribute('src', url);
 
 window.onresize = function() {
   width = window.innerWidth;
--- public/videos.js
+++ public/videos.js
@@ -1,3 +1,4 @@
 const videos = [
 	'https://www.youtube.com/embed/RA2QU4srr5w',
+	'https://www.youtube.com/embed/gTfPgNqf5Ys'
 ]
