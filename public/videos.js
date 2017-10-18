f77d47b7ad67d2568da7d04ba755f30b273696c2
intersim <emily.intersimone@gmail.com>
2017-10-17 23:06:01 -0400

    found more videos

--- public/videos.js
+++ public/videos.js
@@ -1,4 +1,27 @@
 const videos = [
-	'https://www.youtube.com/embed/RA2QU4srr5w',
-	'https://www.youtube.com/embed/gTfPgNqf5Ys'
+	'https://www.youtube.com/embed/RA2QU4srr5w', // 1. Lil bub
+	'https://www.youtube.com/embed/gTfPgNqf5Ys', // 2. Minecraft
+	'https://www.youtube.com/embed/_StgHl92v5Q', // 3. Nick Offerman
+	'https://www.youtube.com/embed/f_SqrXtuSoc', // 4. Fire Log with chemicals
+	'https://www.youtube.com/embed/Ye2gMvRa-iE', // 5. Hallmark channel
+	'https://www.youtube.com/embed/sswqlnmbTiA', // 6. Fire tornado
+	'https://www.youtube.com/embed/VEx4KMzPegc', // 7. Original WPIX
+	'https://www.youtube.com/embed/Lb0eTSE22pM', // 8. BB8
+	'https://www.youtube.com/embed/Dp9C_0_JeYE', // 9. Hearthstone (Blizzard entertinament)
+	'https://www.youtube.com/embed/pCevjyYbEPU', // 10. Mystery Science Theater 3000
+	'https://www.youtube.com/embed/2Vy4KMO0Zk4', // 11. PES (candy corn and pretzels)
+	'https://www.youtube.com/embed/QQ3II0ws350', // 12. Great Big Story
+	'https://www.youtube.com/embed/ZZO0fpjH1Sg', // 13. classic yule log - 5 hours
+	'https://www.youtube.com/embed/83viWKmP9r0', // 14. Josh Groban
+	'https://www.youtube.com/embed/v24316C3Pzg', // 15. dominos
+	'https://www.youtube.com/embed/qXUSRC-ViN8', // 16. historic home in Cleveland
+	'https://www.youtube.com/embed/MBdQ3xGguUQ', // 17. chocolate
+	'https://www.youtube.com/embed/nX9Ve90RUgE', // 18. brussels griffon
+	'https://www.youtube.com/embed/MrZmt6_lV6g', // 19. Hobbiton-esque
+	'https://www.youtube.com/embed/ya4s9UwDnDI', // 20. boston terrier
+	'', // 21.
+	'', // 22.
+	'', // 23.
+	'', // 24.
+	'' // 25.
 ]
