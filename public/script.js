2231ea902919a677328e8bd6d4d7cf69bb9856a6
intersim <emily.intersimone@gmail.com>
2017-10-17 23:19:24 -0400

    remove josh groban

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ const [ iframe ] = Array.from(document.getElementsByTagName('iframe'));
 iframe.setAttribute('width', width);
 iframe.setAttribute('height', height);
 
-const random = Math.ceil(Math.random() * 20); 
+const random = Math.ceil(Math.random() * 19); 
 
 const url = videos[random] + '?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1';
 
--- public/videos.js
+++ public/videos.js
@@ -12,14 +12,14 @@ const videos = [
 	'https://www.youtube.com/embed/2Vy4KMO0Zk4', // 11. PES (candy corn and pretzels)
 	'https://www.youtube.com/embed/QQ3II0ws350', // 12. Great Big Story
 	'https://www.youtube.com/embed/ZZO0fpjH1Sg', // 13. classic yule log - 5 hours
-	'https://www.youtube.com/embed/83viWKmP9r0', // 14. Josh Groban
+	'https://www.youtube.com/embed/rpnBVISuMbg', // 14. historic home in Westchester
 	'https://www.youtube.com/embed/v24316C3Pzg', // 15. dominos
 	'https://www.youtube.com/embed/qXUSRC-ViN8', // 16. historic home in Cleveland
 	'https://www.youtube.com/embed/MBdQ3xGguUQ', // 17. chocolate
 	'https://www.youtube.com/embed/nX9Ve90RUgE', // 18. brussels griffon
 	'https://www.youtube.com/embed/MrZmt6_lV6g', // 19. Hobbiton-esque
 	'https://www.youtube.com/embed/ya4s9UwDnDI', // 20. boston terrier
-	'https://www.youtube.com/embed/rpnBVISuMbg', // 21. historic home in Westchester
+	'', // 21.
 	'', // 22.
 	'', // 23.
 	'', // 24.
