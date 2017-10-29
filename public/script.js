4d7c6b833927ae0f8a3187f94483a16ddb3dd0dd
intersim <emily.intersimone@gmail.com>
2017-10-28 22:10:40 -0400

    added numbers with basic styling and functionality

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// create video
 let width = window.innerWidth;
 let height = window.innerHeight;
 
@@ -20,4 +21,22 @@ window.onresize = function() {
 
   iframe.setAttribute('width', width);
   iframe.setAttribute('height', height);
+}
+
+// create numbers
+
+const numbersContainer = document.getElementById('numbers');
+
+function changeVideoCallbackMaker (number) {
+  return () => {
+    const url = videos[number] + '?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1';
+    iframe.setAttribute('src', url)
+  }
+}
+
+for (let i = 0; i <= 24; i++) {
+  const dayElement = document.createElement('span');
+  dayElement.innerHTML = i + 1;
+  dayElement.onclick = changeVideoCallbackMaker(i);
+  numbersContainer.appendChild(dayElement);
 }
